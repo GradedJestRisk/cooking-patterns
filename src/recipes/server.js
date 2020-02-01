d79fb094030278bbb385fed806255480dc425d58
TOP Pierre (TOPI) <ptop@octo.com>
2020-02-01 17:34:01 +0100

    API: Retrieve recipe, remove /

--- src/recipes/server.js
+++ src/recipes/server.js
@@ -4,17 +4,24 @@ const port = process.env.PORT;
 
 const { createServer } = require('http');
 const router = require('find-my-way')();
-const { recipesList } = require('./recipe');
-
-router.get('/', (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' });
-  response.write(JSON.stringify({ resources: 'recipe' }));
-  response.end();
-});
+const { recipes } = require('./recipe');
 
 router.get('/recipes', (request, response) => {
   response.writeHead(200, { 'Content-Type': 'application/json' });
-  response.write(JSON.stringify(recipesList));
+  response.write(JSON.stringify(recipes));
+  response.end();
+});
+
+router.get('/recipes/:id', (request, response, params) => {
+  const recipeId = params.id;
+  const recipeIndex = recipes.findIndex((r) => (r.id === recipeId));
+  if (recipeIndex === -1) {
+    response.writeHead(404, { 'Content-Type': 'application/json' });
+    response.write(JSON.stringify({ error: `recipe ${recipeId} not found` }));
+  } else {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.write(JSON.stringify(recipes[recipeIndex]));
+  }
   response.end();
 });
 
