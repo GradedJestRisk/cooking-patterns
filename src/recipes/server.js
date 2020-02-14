580334e3c51b943794f1ae46ea08adc14bcd4f80
GradedJestRisk <graded.jest.risk@gmail.com>
2020-02-14 11:18:59 +0100

    TECH: Add request validation - Add missing properties rejection - Improve code (structural)

--- src/recipes/server.js
+++ src/recipes/server.js
@@ -8,13 +8,17 @@ const router = require('find-my-way')();
 const parse = require('co-body');
 const { recipes } = require('./recipe');
 
-const unauthorizedResponseCode = 400;
-const createdResponseCode = 201;
-const notFoundErrorCode = 404;
-const successStatusCode = 200;
+const responseCode = {
+  created: 201,
+  success: 200,
+  error: {
+    unauthorized: 400,
+    notFound: 404,
+  },
+};
 
 router.get('/recipes', (request, response) => {
-  response.writeHead(successStatusCode, { 'Content-Type': 'application/json' });
+  response.writeHead(responseCode.success, { 'Content-Type': 'application/json' });
   response.write(JSON.stringify(recipes));
   response.end();
 });
@@ -23,10 +27,10 @@ router.get('/recipes/:id', (request, response, params) => {
   const recipeId = params.id;
   const recipeIndex = recipes.findIndex((r) => (r.id === recipeId));
   if (recipeIndex === -1) {
-    response.writeHead(notFoundErrorCode, { 'Content-Type': 'application/json' });
+    response.writeHead(responseCode.error.notFound, { 'Content-Type': 'application/json' });
     response.write(JSON.stringify({ error: `recipe ${recipeId} not found` }));
   } else {
-    response.writeHead(successStatusCode, { 'Content-Type': 'application/json' });
+    response.writeHead(responseCode.success, { 'Content-Type': 'application/json' });
     response.write(JSON.stringify(recipes[recipeIndex]));
   }
   response.end();
@@ -36,15 +40,16 @@ router.get('/recipes/:id', (request, response, params) => {
 router.post('/recipes', (request, response) => {
   parse(request)
     .then((body) => {
-      if (!body.name || !body.description) {
-        response.writeHead(unauthorizedResponseCode);
+      const receivedRecipe = body;
+      if (!receivedRecipe.name || !receivedRecipe.description) {
+        response.writeHead(responseCode.error.unauthorized);
         response.write(JSON.stringify({ error: 'recipe must include the following properties: name, description' }));
       } else {
         const id = 3;
         const location = `${API_URL + request.url}/${id}`;
-        response.writeHead(createdResponseCode, { 'Content-Type': 'application/json', location });
-        const recipe = { ...body, id };
-        response.write(JSON.stringify(recipe));
+        response.writeHead(responseCode.created, { 'Content-Type': 'application/json', location });
+        const createdRecipe = { ...receivedRecipe, id };
+        response.write(JSON.stringify(createdRecipe));
       }
     })
   // eslint-disable-next-line no-console
